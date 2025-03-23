d93c48396ca3f0e3b072d54765a3d290b9d1629e
Italo Gois <italo.alberto26@gmail.com>
2025-03-23 20:44:15 -0300

    Refactor hello function and expand tests to support multiple languages

--- hello.go
+++ hello.go
@@ -2,11 +2,30 @@ package main
 
 import "fmt"
 
-func hello() string {
-	return "Hello, World!"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const spanish = "Spanish"
+const french = "French"
+const frenchHelloPrefix = "Bonjour, "
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	prefix := englishHelloPrefix
+
+	if language == spanish {
+		prefix = spanishHelloPrefix
+	}
+
+	if language == french {
+		prefix = frenchHelloPrefix
+	}
+
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(hello())
+	fmt.Println(Hello("Man", "Spanish"))
 }
 
